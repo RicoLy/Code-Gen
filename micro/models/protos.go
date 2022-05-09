6d5674fefd0e3a21f5d0ea3db7d113b9ef6823f4
v_ricolyluo <v_ricolyluo@tencent.com>
2022-05-09 22:18:24 +0800

    -

--- micro/models/protos.go
+++ micro/models/protos.go
@@ -25,28 +25,30 @@ type Message struct {
 	ElementInfos []ElementInfo // proto字段
 }
 
-// 结构体信息
+// 结构体信息 message\s*(\w*)\s*{([\s\|\*\/@\w:",=;]*)}
 type StructInfo struct {
 	StructName   string
 	ElementInfos []ElementInfo // golang字段
 	HasWrite     bool          // 是否写入文件
 }
 
-// 字段信息
+// 字段信息 \/\/[\s\w@:",|=*]*;
+// \s*(\w*)\s*(\w*)\s*=\s*(\w*)\s*;
+// \w*:"[\w\|\*=]*"
 type ElementInfo struct {
 	Name string            // 名称
 	Type string            // golang 数据类型
 	Tags map[string]string // 标签信息 tag | value  json:"pid" form:"pid"
 }
 
-// 方法信息 rpc[\s]*([\w]*)\((\w*)\)\s*\w*\s*\((\w*)\)
+// 方法信息 rpc[\s]*([\w]*)\((\w*)\)\s*\w*\s*\((\w*)\)\s*{}
 type Method struct {
 	MethodName string // 方法名
 	Param      string // 参数
 	Returns    string // 返回值
 }
 
-// 服务信息  service\s*(\w*)\s*{
+// 服务信息  service\s*(\w*)\s*{[\s\w(){};]*}
 type ProtoService struct {
 	ServiceName string
 	Methods     []Method
